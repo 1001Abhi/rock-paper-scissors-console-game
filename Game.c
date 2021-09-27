bac5a23437e5b3579d9cc1b2f83d7a2fc7e5c186
1001Abhi <91471645+1001Abhi@users.noreply.github.com>
2021-09-27 18:21:29 +0530

    Add files via upload

---
+++ Game.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+char Rockpaperscissors(char u , char c){
+    if(u == c){
+        return 'D';
+    }
+    if(u == 'R' && c == 'S'){
+         return 'W';
+    }
+    if(u == 'S' && c == 'R'){
+        return 'L';
+    }
+         
+    if(u == 'P' && c == 'R'){
+        return 'W';
+    }
+    if(u == 'R' && c == 'P'){
+        return 'L';
+    }
+        
+    if(u == 'S' && c == 'P'){
+        return 'W';
+    }
+    if(u == 'P' && c == 'S'){
+        return 'L';
+    }
+        
+}
+
+int main(){
+    char computer, yourself;
+    srand(time(0));
+    int number = rand()%100 + 1;
+    if(number < 33){
+       computer = 'P'; 
+    }
+    else if(number > 33 && number < 66){
+        computer = 'R';
+    }
+    else{
+        computer = 'S';
+    }
+    printf("Enter Rock(R) , Paper(P) & scissors(S)\n");
+    scanf("%c",&yourself);
+    printf("You chooses %c and Computer chooses %c \n", yourself, computer);
+    char result = Rockpaperscissors(yourself, computer);
+    if(result == 'W'){
+        printf("You Won\n");
+    }
+    else if(result == 'L'){
+        printf("You Lose\n");
+    }
+    if(result == 'D'){
+        printf("Draw");
+    }
+
+}
